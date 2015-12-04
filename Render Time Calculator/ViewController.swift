234b615e7f8bfecd46b8c4ae6bc752bb747c6d33
tomtatchell <tom.tatchell@googlemail.com>
2015-12-04 17:02:43 +0000

    Render Time Calc Initial Commit
    Initial Commit
    
    A small in house tool developed in python then translated to swift for
    calculating remaining render times on projects.

--- Render Time Calculator/ViewController.swift
+++ Render Time Calculator/ViewController.swift
@@ -10,6 +10,48 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var fTimeMin: NSTextField!
+    @IBOutlet var fTimeSec: NSTextField!
+    @IBOutlet var renderFrames: NSTextField!
+    
+    @IBOutlet var timeRemaining: NSTextField!
+    
+    @IBAction func calc(sender: NSButton) {
+        
+        let timeInSec = ((fTimeMin.integerValue * 60) + fTimeSec.integerValue)
+        let totalTime = (timeInSec * renderFrames.integerValue)
+        let totalSec = totalTime
+        let totalMins = totalSec / 60
+        let totalHrs = totalMins / 60
+
+        /*
+        if totalMins > 60 {
+            
+            timeRemaining.stringValue = ("Time Reamining: \(totalHrs) : \(totalMins - (totalHrs * 60)) : \(totalSec - (totalMins * 60))")
+            
+        } else {
+            
+            timeRemaining.stringValue = ("Time Remaining: \(totalMins) : \(totalSec - (totalMins * 60))")
+            
+        }
+        */
+        
+        let printSec = totalSec - (totalMins * 60)
+        let printMins = totalMins - (totalHrs * 60)
+        let printHrs = totalHrs
+        
+        timeRemaining.stringValue = String(format: "Time Reamining: %02d:%02d:%02d", arguments: [printHrs, printMins, printSec])
+        
+        
+        
+    }
+
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
