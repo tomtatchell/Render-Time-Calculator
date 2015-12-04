8a6423f40c2e4c12e0e6ef65a4ae50b1c3a48d3f
tomtatchell <tom.tatchell@googlemail.com>
2015-12-04 15:05:30 +0000

    Initial Commit

---
+++ Render Time Calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Render Time Calculator
+//
+//  Created by BBMP03 on 04/12/2015.
+//  Copyright © 2015 tomtatchell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Render Time Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Render Time Calculator
+//
+//  Created by BBMP03 on 04/12/2015.
+//  Copyright © 2015 tomtatchell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
